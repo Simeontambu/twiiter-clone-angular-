217a2f909ee8e1a65716950c99207f3c94891936
simeontambu <simeontambu@gmail.com>
2024-03-06 20:12:13 +0100

    Feat:implementation of sidebar right

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarLeftComponent } from './sidebar-left/sidebar-left.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarLeftComponent],
+  imports: [RouterOutlet, SidebarLeftComponent, SidebarRightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
